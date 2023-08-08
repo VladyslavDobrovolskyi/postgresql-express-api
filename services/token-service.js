5467d11b52f5c62a27c216033cfa936d2d821849
VladyslavDobrovolskyi <vladyslav.dobrovolski@gmail.com>
2023-08-08 17:39:54 +0300

    Token logic & validation

--- services/token-service.js
+++ services/token-service.js
@@ -1,1 +1,19 @@
-//TODO Генерация токена на 40 минут и с 1 использованием
+const { TOKEN_ACCESS_SECRET } = require('../config')
+const jwt = require('jsonwebtoken')
+const TokenStorage = new Set() //INFO При перезагрузке сервера сторейдж сбрасывается и можно зарегистрировать еще раз на токен, но если он не протух
+
+class TokenService {
+  async generateToken(payload = Date.now()) {
+    const token = jwt.sign({ payload }, TOKEN_ACCESS_SECRET, {
+      expiresIn: '40m',
+    })
+    return token
+  }
+  useToken(token) {
+    if (!TokenStorage.has(token)) {
+      TokenStorage.add(token)
+    } else throw new Error('Token expired.')
+  }
+}
+
+module.exports = new TokenService()
