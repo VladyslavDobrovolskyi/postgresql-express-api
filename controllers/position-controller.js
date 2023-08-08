1072ac1d96d6d4ebab017444071e6a0c9b9f04ee
VladyslavDobrovolskyi <vladyslav.dobrovolski@gmail.com>
2023-08-08 18:54:24 +0300

    valid pos

--- controllers/position-controller.js
+++ controllers/position-controller.js
@@ -4,7 +4,12 @@ class PositionController {
   async getPositions(req, res) {
     try {
       const positions = await PositionService.getPositions()
-      res.json({ success: 'true', posistions: positions })
+      if (positions) res.json({ success: 'true', posistions: positions })
+      else
+        res.status(404).json({
+          success: false,
+          message: 'Positions not found.',
+        })
     } catch (error) {
       res.json({ error: error.message })
     }
