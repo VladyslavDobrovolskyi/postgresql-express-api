6f9ddb37f4e0576462b9e99632130280d74318cf
VladyslavDobrovolskyi <vladyslav.dobrovolski@gmail.com>
2023-08-08 18:55:53 +0300

    -improve (!)

--- middlewares/uniqueEmailNumber-Middleware.js
+++ middlewares/uniqueEmailNumber-Middleware.js
@@ -4,20 +4,22 @@ async function uniqueEmailNumber(req, res, next) {
   const { email, phone } = req.body
   try {
     const user = await db.query(
-      'SELECT * FROM users WHERE email = $1 OR phone = $2 LIMIT 1;',
+      'SELECT * FROM users WHERE email = $1 OR phone = $2 LIMIT 1;', //TODO обработчик ошибок на этот запрос?
       [email, phone]
     )
     if (user.rowCount > 0) {
-      console.log(user)
-      throw new Error()
+      res.status(409).json({
+        success: false,
+        message: 'User with this phone or email already exist',
+      })
     }
-    next()
   } catch (error) {
-    return res.status(409).json({
-      success: 'false',
-      message: 'User with this phone or email already exist',
+    console.error('Error executing query', error)
+    res.status(500).json({
+      error: 'Internal Server Error',
     })
   }
+  next()
 }
 
 module.exports = uniqueEmailNumber
