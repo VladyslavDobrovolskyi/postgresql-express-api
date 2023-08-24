d646a74bd50a0a46188cdca1cf9b77f98453f696
VladyslavDobrovolskyi <vladyslav.dobrovolski@gmail.com>
2023-08-24 15:51:35 +0300

    TokenStorage update

--- services/token-service.js
+++ services/token-service.js
@@ -1,9 +1,11 @@
 import jwt from 'jsonwebtoken'
 import { TOKEN_ACCESS_SECRET } from '../config.js'
 
-const TokenStorage = new Set()
-
 class TokenService {
+  constructor() {
+    this.TokenStorage = new Set()
+  }
+
   async generateToken(payload = Date.now()) {
     const token = jwt.sign({ payload }, TOKEN_ACCESS_SECRET, {
       expiresIn: '40m',
@@ -12,8 +14,8 @@ class TokenService {
   }
 
   useToken(token) {
-    if (!TokenStorage.has(token)) {
-      TokenStorage.add(token)
+    if (!this.TokenStorage.has(token)) {
+      this.TokenStorage.add(token)
     } else {
       throw new Error('Token expired.')
     }
